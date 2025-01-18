ca56d291d1ce9dd0637bd03e1b59a71d7b1c1512
Matthew <Mattmicgid@gmail.com>
2025-01-18 23:53:48 +0700

    ya gitulah

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/sections/Header";
 import About from "@/sections/About";
+import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return <>
   <Header/> 
   <About/>
+  <Footer/>
   </>;
 }
